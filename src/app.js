2e4f48e27551f1366903a49cd2e78ecc055d7745
electroninio <javier.gerol@gmail.com>
2024-09-19 23:47:10 -0300

    servicios estaticos

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 import express from 'express';
-
+import path from 'path';
+import { fileURLToPath } from 'url';
 // only for test db connection
 import './config/db.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
