b4fe3969946d74aa337d368aef442bd3a4b8830b
electroninio <javier.gerol@gmail.com>
2024-11-16 21:30:24 -0300

    agrego el procedure

---
+++ docs/sql/stored-procedures/SP - user_statics_by_office.sql
@@ -0,0 +1,19 @@
+DELIMITER $$
+
+CREATE PROCEDURE userStaticsByOffice()
+BEGIN
+   SELECT 
+    oficinas.idOficina AS idOficina,
+    oficinas.nombre AS nombre,
+    COUNT(usuarios_oficinas.idOficina) AS Cantidad_Usuarios_Activos
+FROM 
+    oficinas
+LEFT JOIN 
+    usuarios_oficinas ON oficinas.idOficina = usuarios_oficinas.idOficina AND usuarios_oficinas.activo = 1
+GROUP BY 
+    oficinas.idOficina, oficinas.nombre
+ORDER BY 
+    Cantidad_Usuarios_Activos DESC;
+END$$
+
+DELIMITER ;
